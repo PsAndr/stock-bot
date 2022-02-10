703a860128a5d6ebb8c6e3dfa631315e256061f1
PsAndr <96647628+PsAndr@users.noreply.github.com>
2022-02-10 16:10:08 +0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -14,7 +14,7 @@ S_TOKEN = "t.gJWIDbsjDOGnbAl2y-pm5kzEIxljV-kWYb1To6Skr4STriOvfDp4q4xwvFzuLzaXxWZ
 client = tinvest.SyncClient(TOKEN)'''
 
 spis = []
-g = Github('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')
+g = Github('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')
 repo = g.get_user().get_repo("stock-bot")
 contents = repo.get_contents("save.txt")
 
