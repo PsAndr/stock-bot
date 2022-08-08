df89af25a14ababb0abb3896be4fb166cc8b08e8
PsAndr <96647628+PsAndr@users.noreply.github.com>
2022-08-09 02:44:01 +0500

    Update __init__.py

--- can_buy/__init__.py
+++ can_buy/__init__.py
@@ -2,8 +2,6 @@ from tradingview_ta import TA_Handler, Interval, Exchange
 import tinvest
 
 def can_buy(el : str):
-    TOKEN = "t.WVpg6thNk00O9Vd8P4vrne6om7zDgWaGIsKH6TqdRKgT2giER_3Lqp7w9DI7NYdjPWF4AXkj6MRNP5G51zp2lQ"
-    S_TOKEN = "t.gJWIDbsjDOGnbAl2y-pm5kzEIxljV-kWYb1To6Skr4STriOvfDp4q4xwvFzuLzaXxWZt2UzRXysejROedAS1TQ"
     client = tinvest.SyncClient(TOKEN)
     instr = client.get_market_search_by_ticker(el)
     fg = instr.payload.instruments[0].figi
